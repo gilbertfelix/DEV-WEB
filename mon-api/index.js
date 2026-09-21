c2ae62aa22db49cd770d66de8fc20de845e23e79
gilbertfelix <125877738+gilbertfelix@users.noreply.github.com>
2026-09-21 17:07:12 +0200

    Modifincation des fichiers

--- mon-api/index.js
+++ mon-api/index.js
@@ -3,11 +3,10 @@ import express from "express" /* Pour utiliser les modules ES6, on doit ajouter 
 
 function cors(req,res,next){
     res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Methodes', '*');
+    res.set('Access-Control-Allow-Methods', '*');
     res.set('Access-Control-Allow-Headers', 'Content-Type, x-api-key');
     next();
 }
-app.u
 const app = express() // Création d'une instance de l'application Express
 app.use(express.json()) // L'application utilise express json pour parser les requêtes entrantes avec un payload JSON
 app.use(cors);
@@ -18,5 +17,5 @@ app.get("/test/:id", (req, res) => {
 
 fetch(`https://jsonplaceholder.typicode.com/posts/1`) // On fait un fetch pour récupérer les données d'un site externe
     .then(res => res.json()) // On parse la réponse en JSON
-    .then(data => res.log(data)); // On renvoie les données au client
+    .then(data => console.log(data));
 app.listen(3000, () => console.log("http://localhost:3000"))
--- mon-api/my-app/src/components/button.jsx
+++ mon-api/my-app/src/components/button.jsx
@@ -1,34 +1,36 @@
-import {useState} from 'react';
+import {useEffect,useState} from 'react';
 
-const Title =() => {
-    const Apicall = async () => {
+const Title = () => {
     const [count, setCount] = useState(0);
-    const [titre, setTitre] = useState("Hello word");
-    const reponse = await fetch('https://dev-web-1xz5.onrender.com/')
-    const fdata = await reponse.json();
-    console.log(data);
-    setTitre(data.body);
-}
- useeffect(() => {
-    Apicall()
- }, [])
+    const [titre, setTitre] = useState("Hello world");
+
+    const Apicall = async () => {
+        const reponse = await fetch('https://dev-web-1xz5.onrender.com/')
+        const fdata = await reponse.json();
+        console.log(fdata);
+        setTitre(fdata.body);
+    }
+
+    useEffect(() => {
+        Apicall()
+    }, [])
 
     return (
-    <div className="george">
-        <h1>{titre}</h1>
-        <p>Ceci est un composant React tout simple.</p>
-        <p>{count}</p>
+        <div className="george">
+            <h1>{titre}</h1>
+            <p>Ceci est un composant React tout simple.</p>
+            <p>{count}</p>
 
-        <button onClick={() => {setCount(count + 1)}}>
-            Cliquez sur moi
-        </button>
+            <button onClick={() => {setCount(count + 1)}}>
+                Cliquez sur moi
+            </button>
 
-        <button onClick={() => {setTitre("Le titre a changé.")}}>
-            Click pour changer le titre 
-        </button>
-       
-    </div>
+            <button onClick={() => {setTitre("Le titre a changé.")}}>
+                Click pour changer le titre
+            </button>
 
-)
+        </div>
+    )
+}
 
 export default Title;
